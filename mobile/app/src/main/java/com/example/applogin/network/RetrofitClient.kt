a557c7aebef4262853cdc4abe298c75527f2712f
Jimmy Alexander Huerta Vasquez <2123010236@untels.edu.pe>
2025-07-24 04:45:23 -0500

    variables de entorno libres

--- mobile/app/src/main/java/com/example/applogin/network/RetrofitClient.kt
+++ mobile/app/src/main/java/com/example/applogin/network/RetrofitClient.kt
@@ -4,9 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
+    // Para producción (Railway)
+    private const val BASE_URL = "https://mylogin-production.up.railway.app/api/"
 
-    // Para emulador Android -> localhost del PC es 10.0.2.2
-    private const val BASE_URL = "http://10.0.2.2:8075/api/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
